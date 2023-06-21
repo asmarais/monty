e1ccc7f64a9d219adc4dbc3f08481fad53b3ab51
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-21 20:49:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,16 +11,37 @@ int main(int argc, char **argv)
 {
 	FILE *f;
 	stack_t *stack = NULL;
+	char *lineptr = NULL;
+	size_t n, read;
 
+	gvars.line_number = 0;
+	gvars.cmd = NULL;
+	
+	
 	if (argc != 2)
 	{
 		fprintf(stdout, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	f = fopen(argv[1], "r");
-	if (fp == NULL)
+	if (f == NULL)
 	{
 		fprintf(stdout, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
+	}
+	read = getline(&lineptr, &n, f);
+	while (read != -1)
+	{
+		gvars.line_number++;
+		token = tokenize(lineptr);
+		if (token != -1)
+		{
+			check = find_op(&stack);
+			if(check == -1)
+			{
+				exit(EXIT_FAILURE);
+			}
+		}
+		read = getline(&lineptr, &n, f);	
 	}
 }
