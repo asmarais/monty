83bfcba6adf4648b6bc7bd2c8d61182fd219aca0
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-06-22 13:00:44 +0100

    Create _free.c

---
+++ _free.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_dlistint - Frees a linked stack_t list.
+ * @head: The head of the stack_t list.
+ */
+
+void _free(stack_t *head)
+{
+	stack_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
