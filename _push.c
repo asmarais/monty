959cdc6e6c2d645ce294b4365f42e070ee9cc087
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-22 17:29:54 +0100

    Update _push.c

--- _push.c
+++ _push.c
@@ -13,7 +13,7 @@ void _push(stack_t **stack, unsigned int line_number)
 	int n;
 	char *param = gvars.raw_input;
 
-	if (param == NULL || check_val(param) == -1)
+	if (check_val(param) == -1)
 	{
 		printf("L<%d>: usage: push integer1\n", line_number);
 		exit(EXIT_FAILURE);
