102faf4634f66dccef3931aa04168af816542a59
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-22 18:24:08 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -8,12 +8,12 @@
 
 int tokenize(char *str)
 {
-	char *delim = " \r\t";
+	char *delim = " \r\t\n";
 
-	if (!str)
-		return (-1);
+	/*if (!str)*/
+	/*	return (-1);*/
 	gvars.cmd = strtok(str, delim);
-	if (gvars.cmd)
+	if (gvars.cmd != NULL)
 	{
 	        gvars.raw_input= strtok(NULL, delim);
 		if (gvars.raw_input)
