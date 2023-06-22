3d8dc10db5a90fcc20e3a43605d28db9c97370fd
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-22 17:48:00 +0100

    Update _pall.c

--- _pall.c
+++ _pall.c
@@ -3,16 +3,15 @@
 /**
  * _pall - prints the value at the top of the stack, followed by a new line
  * @stack: dlistint_t
+ * @line_number:  unsigned int for line number(void in this function)
+ * Return: nothing
 */
 
 void _pall(stack_t **stack,__attribute__((unused))unsigned int line_number)
 {
 	stack_t *current = *stack;
 
-	if (stack == NULL)
-		return;
-
-	while (current)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
