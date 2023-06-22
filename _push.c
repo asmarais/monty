3f6ee7bcf202c5a19302b6e06353d49097d1110f
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-22 18:10:50 +0100

    Update _push.c

--- _push.c
+++ _push.c
@@ -10,9 +10,10 @@
 void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
-	int n;
+	int n, i;
 	char *param = gvars.raw_input;
-	printf("%s",param);
+	for (i = 0; program[i] != '\0';i++)
+	printf("%c",param[i]);
 	printf("%d", check_val(param));
 	if (param == NULL || check_val(param) == -1)
 	{
